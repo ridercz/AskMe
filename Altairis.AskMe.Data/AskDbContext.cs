064f48552befcfff5dd89e71d9b7d48e4f800669
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 00:35:30 +0100

    Initialize DB context

--- Altairis.AskMe.Data/AskDbContext.cs
+++ Altairis.AskMe.Data/AskDbContext.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Altairis.AskMe.Data {
     public class AskDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int> {
+        private const int SeedCategoryCount = 5;            // Number of categories
+
+        // Constructor
 
         public AskDbContext(DbContextOptions<AskDbContext> options)
             : base(options) { }
+
+        // Entities
 
         public DbSet<Question> Questions { get; set; }
 
         public DbSet<Category> Categories { get; set; }
 
+        // Seeding
 
+        public void Seed() {
+            // Add categories
+            if (!this.Categories.Any()) {
+                for (int i = 1; i <= SeedCategoryCount; i++) {
+                    this.Categories.Add(new Category { Name = $"Category {i}" });
+                }
+            }
+        }
     }
 }
--- Altairis.AskMe.Web/Startup.cs
+++ Altairis.AskMe.Web/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Altairis.AskMe.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -25,16 +27,27 @@ namespace Altairis.AskMe.Web {
         }
 
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<AskDbContext>(options => {
+                options.UseSqlite(_config.GetConnectionString("AskDB"));
+            });
+
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AskDbContext context) {
+            // Show detailed errors in development environment
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Migrate database to last version
+            context.Database.Migrate();
+
+            // Seed if in development environment
+            if(env.IsDevelopment()) {
+                context.Seed();
+            }
+
         }
 
     }
