1cf2fa914015ef238d241a9dd51107c20ca48eb1
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 01:18:46 +0100

    Initialize common services and database

--- Altairis.AskMe.Web/Startup.cs
+++ Altairis.AskMe.Web/Startup.cs
@@ -6,6 +6,7 @@ using Altairis.AskMe.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,13 +28,37 @@ namespace Altairis.AskMe.Web {
         }
 
         public void ConfigureServices(IServiceCollection services) {
+            // Configure DB context
             services.AddDbContext<AskDbContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("AskDB"));
             });
 
+            // Configure Razor Pages
+            services.AddMvc()
+                .AddRazorPagesOptions(options => {
+                    options.Conventions.AuthorizeFolder("/Admin");
+                });
+
+            // Configure identity and authentication
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options => {
+                options.Password.RequiredLength = 12;
+                options.Password.RequiredUniqueChars = 4;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<AskDbContext>()
+                .AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(options => {
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Account/Logout";
+                options.SlidingExpiration = true;
+                options.ExpireTimeSpan = TimeSpan.FromDays(30);
+            });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AskDbContext context) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AskDbContext context, UserManager<ApplicationUser> userManager) {
             // Show detailed errors in development environment
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
@@ -43,11 +68,32 @@ namespace Altairis.AskMe.Web {
             // Migrate database to last version
             context.Database.Migrate();
 
-            // Seed if in development environment
-            if(env.IsDevelopment()) {
+            // Seed initial data if in development environment
+            if (env.IsDevelopment()) {
+                // Create categories
                 context.Seed();
+
+                // Create default user
+                if (!userManager.Users.Any()) {
+                    var adminUser = new ApplicationUser { UserName = "admin" };
+                    var r = userManager.CreateAsync(adminUser, "pass.word123").Result;
+                    if (r != IdentityResult.Success) {
+                        var errors = string.Join(", ", r.Errors.Select(x => x.Description));
+                        throw new Exception("Seeding default user failed: " + errors);
+                    }
+                }
             }
 
+            // Enable static file caching for one year
+            app.UseStaticFiles(new StaticFileOptions {
+                OnPrepareResponse = ctx => {
+                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=31536000");
+                }
+            });
+
+            // Use other middleware
+            app.UseAuthentication();
+            app.UseMvc();
         }
 
     }
