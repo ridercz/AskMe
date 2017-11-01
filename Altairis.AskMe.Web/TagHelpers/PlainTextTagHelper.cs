e993df3c215586fab575daf764d5b78f6a7175a6
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 19:24:36 +0100

    Added ParagraphFormatString

--- Altairis.AskMe.Web/TagHelpers/PlainTextTagHelper.cs
+++ Altairis.AskMe.Web/TagHelpers/PlainTextTagHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -21,6 +22,8 @@ namespace Altairis.AskMe.Web.TagHelpers {
 
         public string ContainerTagName { get; set; } = string.Empty;
 
+        public string ParagraphFormatString { get; set; } = "<p>{0}</p>";
+
         public override void Process(TagHelperContext context, TagHelperOutput output) {
             base.Process(context, output);
             if (string.IsNullOrWhiteSpace(this.Text)) return;
@@ -29,11 +32,7 @@ namespace Altairis.AskMe.Web.TagHelpers {
             var paragraphs = this.Text.Split(Environment.NewLine);
             var sb = new StringBuilder();
             foreach (var line in paragraphs) {
-                if (this.HtmlEncode) {
-                    sb.AppendLine("<p>" + _encoder.Encode(line) + "</p>");
-                } else {
-                    sb.AppendLine("<p>" + line + "</p>");
-                }
+                sb.AppendLine(string.Format(this.ParagraphFormatString, this.HtmlEncode ? _encoder.Encode(line) : line));
             }
 
             // Return outpuut
