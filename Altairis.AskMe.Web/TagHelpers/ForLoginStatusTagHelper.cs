9e2704f7058b084dee7094ea1e253770cd60aa49
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 23:09:33 +0100

    Added login status conditional tag helper

---
+++ Altairis.AskMe.Web/TagHelpers/ForLoginStatusTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Altairis.AskMe.Web.TagHelpers {
+    [HtmlTargetElement(Attributes ="for-login-status")]
+    public class ForLoginStatusTagHelper : TagHelper {
+        private readonly HttpContext _httpContext;
+
+        public ForLoginStatusTagHelper(IHttpContextAccessor httpContextAccessor) {
+            _httpContext = httpContextAccessor.HttpContext;
+        }
+
+        public bool ForLoginStatus { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            base.Process(context, output);
+
+            if(_httpContext.User.Identity.IsAuthenticated != this.ForLoginStatus) {
+                output.SuppressOutput();
+            }
+        }
+
+    }
+}
