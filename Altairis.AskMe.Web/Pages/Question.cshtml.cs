fb466ca76c3dd4aeaf8b300bab366358bbfca793
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 23:49:39 +0100

    Added question detail

---
+++ Altairis.AskMe.Web/Pages/Question.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Altairis.AskMe.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Altairis.AskMe.Web.Pages {
+    public class QuestionModel : PageModel {
+        private readonly AskDbContext _dc;
+
+
+        // Constructor
+
+        public QuestionModel(AskDbContext dc) {
+            _dc = dc;
+        }
+
+        // Model properties
+
+        public Question Data { get; set; }
+
+        // Handlers
+
+        public async Task<IActionResult> OnGetAsync(int questionId) {
+            this.Data = await _dc.Questions.Include(x => x.Category).SingleOrDefaultAsync(x => x.Id == questionId);
+            if (this.Data == null) return this.NotFound();
+            return this.Page();
+        }
+    }
+}
