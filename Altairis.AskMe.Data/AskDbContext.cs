83827ffd54dff66d7623d139803915b01cfab49b
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 15:31:08 +0100

    Added question seeding

--- Altairis.AskMe.Data/AskDbContext.cs
+++ Altairis.AskMe.Data/AskDbContext.cs
@@ -2,10 +2,13 @@
 using System.Linq;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NLipsum.Core;
 
 namespace Altairis.AskMe.Data {
     public class AskDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int> {
-        private const int SeedCategoryCount = 5;            // Number of categories
+        private const int SEED_MAX_QUESTION_PARAGRAPHS = 10;
+        private const int SEED_MAX_ANSWER_PARAGRAPHS = 20;
+        private const int SEED_MAX_NAME_WORDS = 4;
 
         // Constructor
 
@@ -20,13 +23,41 @@ namespace Altairis.AskMe.Data {
 
         // Seeding
 
-        public void Seed() {
+        public void Seed(int numberOfCategories = 5, int numberOfQuestions = 50, int anonymousRate = 75, int answeredRate = 50) {
             // Add categories
             if (!this.Categories.Any()) {
-                for (int i = 1; i <= SeedCategoryCount; i++) {
+                for (int i = 1; i <= numberOfCategories; i++) {
                     this.Categories.Add(new Category { Name = $"Category {i}" });
                 }
+                this.SaveChanges();
             }
+
+            // Add questions
+            if (!this.Questions.Any()) {
+                var categoryIds = this.Categories.Select(c => c.Id).ToArray();
+                var rng = new Random();
+                var rtg = new LipsumGenerator();
+                for (int i = 0; i < numberOfQuestions; i++) {
+                    var nq = new Question {
+                        QuestionText = string.Join(Environment.NewLine, rtg.GenerateParagraphs(rng.Next(SEED_MAX_QUESTION_PARAGRAPHS) + 1)),
+                        DateCreated = DateTime.Now.AddHours(i - numberOfQuestions),
+                        CategoryId = categoryIds[rng.Next(categoryIds.Length)]
+                    };
+                    if (rng.Next(100) > anonymousRate) {
+                        var name = rtg.GenerateWords(rng.Next(SEED_MAX_NAME_WORDS) + 1);
+                        nq.DisplayName = string.Join(' ', name);
+                        nq.EmailAddress = name[0].ToLower() + "@example.com";
+                    }
+                    if (rng.Next(100) <= answeredRate) {
+                        nq.AnswerText = string.Join(Environment.NewLine, rtg.GenerateParagraphs(rng.Next(SEED_MAX_ANSWER_PARAGRAPHS) + 1));
+                        nq.DateAnswered = nq.DateCreated.AddMinutes(rng.Next((int)DateTime.Now.Subtract(nq.DateCreated).TotalMinutes));
+                    }
+                    this.Questions.Add(nq);
+                }
+                this.SaveChanges();
+            }
+
+
         }
     }
 }
