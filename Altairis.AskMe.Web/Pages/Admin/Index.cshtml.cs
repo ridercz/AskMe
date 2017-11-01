304786c29d98dd29aeba3ff25b04fbf62da84f76
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 23:49:17 +0100

    Added question updating

--- Altairis.AskMe.Web/Pages/Admin/Index.cshtml.cs
+++ Altairis.AskMe.Web/Pages/Admin/Index.cshtml.cs
@@ -1,16 +1,96 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Altairis.AskMe.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Options;
 
-namespace Altairis.AskMe.Web.Pages.Admin
-{
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace Altairis.AskMe.Web.Pages.Admin {
+    public class IndexModel : PageModel {
+        private readonly AskDbContext _dc;
+        private readonly AppConfiguration _cfg;
+
+        // Constructor
+
+        public IndexModel(AskDbContext dc, IOptionsSnapshot<AppConfiguration> optionsSnapshot) {
+            _dc = dc;
+            _cfg = optionsSnapshot.Value;
+        }
+
+        // Model properties
+
+        public IEnumerable<SelectListItem> Categories => _dc.Categories
+            .OrderBy(c => c.Name)
+            .Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        // Input model
+
+        public class InputModel {
+            [Required(ErrorMessage = "Není zadána otázka"), MaxLength(500), DataType(DataType.MultilineText)]
+            public string QuestionText { get; set; }
+
+            public string AnswerText { get; set; }
+
+            [MaxLength(100)]
+            public string DisplayName { get; set; }
+
+            [MaxLength(100), DataType(DataType.EmailAddress, ErrorMessage = "Nesprávný formát e-mailové adresy")]
+            public string EmailAddress { get; set; }
+
+            public int CategoryId { get; set; }
+        }
+
+        // Handlers
+
+        public async Task<IActionResult> OnGetAsync(int questionId) {
+            // Get question
+            var q = await _dc.Questions.FindAsync(questionId);
+            if (q == null) return this.NotFound();
+
+            // Prepare model
+            this.Input = new InputModel {
+                AnswerText = q.AnswerText,
+                CategoryId = q.CategoryId,
+                DisplayName = q.DisplayName,
+                EmailAddress = q.EmailAddress,
+                QuestionText = q.QuestionText
+            };
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int questionId) {
+            // Get question
+            var q = await _dc.Questions.FindAsync(questionId);
+            if (q == null) return this.NotFound();
+
+            if (this.ModelState.IsValid) {
+                // Update question
+                q.CategoryId = this.Input.CategoryId;
+                q.DisplayName = this.Input.DisplayName;
+                q.EmailAddress = this.Input.EmailAddress;
+                q.QuestionText = this.Input.QuestionText;
+
+                if (string.IsNullOrWhiteSpace(this.Input.AnswerText)) {
+                    q.AnswerText = null;
+                    q.DateAnswered = null;
+                }
+                else {
+                    q.AnswerText = this.Input.AnswerText;
+                    if (!q.DateAnswered.HasValue) q.DateAnswered = DateTime.Now;
+                }
+
+                await _dc.SaveChangesAsync();
+                return this.RedirectToPage("/Question", new { questionId = q.Id });
+            }
+            return this.Page();
         }
     }
 }
