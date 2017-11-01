31e890ca8b1338b10010137bbcc85cc6812fd899
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 14:39:17 +0100

    Added form styling

--- Altairis.AskMe.Web/Pages/Questions.cshtml.cs
+++ Altairis.AskMe.Web/Pages/Questions.cshtml.cs
@@ -1,16 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Altairis.AskMe.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Altairis.AskMe.Web.Pages
-{
-    public class QuestionsModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace Altairis.AskMe.Web.Pages {
+    public class QuestionsModel : PageModel {
+        private readonly AskDbContext _dc;
+
+        public QuestionsModel(AskDbContext dc) {
+            _dc = dc;
+        }
+
+        public IEnumerable<SelectListItem> Categories => _dc.Categories
+            .OrderBy(c => c.Name)
+            .Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel {
+            [Required(ErrorMessage = "Není zadána otázka"), MaxLength(500), DataType(DataType.MultilineText)]
+            public string QuestionText { get; set; }
+
+            [MaxLength(100)]
+            public string DisplayName { get; set; }
+
+            [MaxLength(100), DataType(DataType.EmailAddress, ErrorMessage = "Nesprávný formát e-mailové adresy")]
+            public string EmailAddress { get; set; }
+
+            public int CategoryId { get; set; }
+        }
+
+        public void OnGet() {
         }
     }
 }
