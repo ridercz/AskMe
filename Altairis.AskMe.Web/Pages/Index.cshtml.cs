0452b4ff1fa4dfe7f34bd28c6df72e638e4496bc
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-01 22:13:59 +0100

    Displaying answered questions

--- Altairis.AskMe.Web/Pages/Index.cshtml.cs
+++ Altairis.AskMe.Web/Pages/Index.cshtml.cs
@@ -2,15 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Altairis.AskMe.Data;
+using Altairis.AskMe.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
-namespace Altairis.AskMe.Web.Pages
-{
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace Altairis.AskMe.Web.Pages {
+    public class IndexModel : PagedPageModel<Question> {
+        private readonly AskDbContext _dc;
+        private readonly AppConfiguration _cfg;
+        private readonly IQueryable<Question> _dataSource;
+
+        // Constructor
+
+        public IndexModel(AskDbContext dc, IOptionsSnapshot<AppConfiguration> optionsSnapshot) {
+            _dc = dc;
+            _cfg = optionsSnapshot.Value;
+            _dataSource = _dc.Questions
+                .Include(x => x.Category)
+                .Where(x => x.DateAnswered.HasValue)
+                .OrderByDescending(x => x.DateAnswered);
         }
+
+        // Handlers
+
+        public async Task OnGetAsync(int pageNumber) {
+            await base.GetData(_dataSource, pageNumber, _cfg.PageSize);
+        }
+
     }
 }
