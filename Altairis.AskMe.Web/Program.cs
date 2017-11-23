b5c54e9a8f77113deb81c5e8aeff02b171e39d36
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-23 22:39:59 +0100

    Prepared setting to launch on all IPs

--- Altairis.AskMe.Web/Program.cs
+++ Altairis.AskMe.Web/Program.cs
@@ -20,6 +20,7 @@ namespace Altairis.AskMe.Web
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                //.UseUrls("http://0.0.0.0:5000")
                 .Build();
     }
 }
