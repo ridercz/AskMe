5f8fba44e8a7cbf7a667bde29aa83018754b9edf
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-11-12 21:12:12 +0100

    Implemented account controller

---
+++ Altairis.AskMe.Web.Mvc/ControllerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altairis.AskMe.Web.Mvc {
+    public static class ControllerExtensions {
+
+        public static IActionResult MessageView(this Controller controller, string title, string message) => controller.View("Message", (title, message));
+
+    }
+}
---
+++ Altairis.AskMe.Web.Mvc/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Altairis.AskMe.Data;
+using Altairis.AskMe.Web.Mvc.Models.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altairis.AskMe.Web.Mvc.Controllers {
+    [Route("account")]
+    public class AccountController : Controller {
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager) {
+            this._signInManager = signInManager;
+        }
+
+        [Route("login")]
+        public IActionResult Login() => this.View();
+
+        [HttpPost, Route("login")]
+        public async Task<IActionResult> Login(LoginModel model, string returnUrl = "/") {
+            if (this.ModelState.IsValid) {
+                var result = await this._signInManager.PasswordSignInAsync(
+                    model.UserName,
+                    model.Password,
+                    model.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded) {
+                    return this.LocalRedirect(returnUrl);
+                } else {
+                    this.ModelState.AddModelError(string.Empty, "Přihlášení se nezdařilo");
+                }
+            }
+            return this.View();
+        }
+
+        [Route("logout")]
+        public async Task<IActionResult> Logout() {
+            await this._signInManager.SignOutAsync();
+            return this.MessageView("Odhlášení", "Byli jste úspěšně odhlášeni");
+        }
+
+    }
+}
