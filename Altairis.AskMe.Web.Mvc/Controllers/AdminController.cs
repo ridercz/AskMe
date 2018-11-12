7020c838f7b5b45602b83aaeeeca1a4178846c7c
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-11-12 22:22:41 +0100

    Implemented question edit

---
+++ Altairis.AskMe.Web.Mvc/Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Altairis.AskMe.Data;
+using Altairis.AskMe.Web.Mvc.Models.Admin;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Altairis.AskMe.Web.Mvc.Controllers {
+
+    [Route("admin"), Authorize]
+    public class AdminController : Controller {
+
+        private readonly AskDbContext _dc;
+
+        // Constructor
+
+        public AdminController(AskDbContext dc) {
+            this._dc = dc;
+        }
+
+        [Route("{questionId:int:min(1)}")]
+        public async Task<IActionResult> Index(int questionId) {
+            // Get question
+            var q = await this._dc.Questions.FindAsync(questionId);
+            if (q == null) return this.NotFound();
+
+            // Prepare model
+            var model = new IndexModel {
+                AnswerText = q.AnswerText,
+                CategoryId = q.CategoryId,
+                DisplayName = q.DisplayName,
+                EmailAddress = q.EmailAddress,
+                QuestionText = q.QuestionText,
+                Categories = await this._dc.Categories
+                    .OrderBy(c => c.Name)
+                    .Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() })
+                    .ToListAsync()
+            };
+
+            return this.View(model);
+        }
+
+        [HttpPost, Route("{questionId:int:min(1)}")]
+        public async Task<IActionResult> Index(int questionId, IndexModel model) {
+            // Get question
+            var q = await this._dc.Questions.FindAsync(questionId);
+            if (q == null) return this.NotFound();
+
+            if (this.ModelState.IsValid) {
+                // Update question
+                q.CategoryId = model.CategoryId;
+                q.DisplayName = model.DisplayName;
+                q.EmailAddress = model.EmailAddress;
+                q.QuestionText = model.QuestionText;
+
+                if (string.IsNullOrWhiteSpace(model.AnswerText)) {
+                    q.AnswerText = null;
+                    q.DateAnswered = null;
+                } else {
+                    q.AnswerText = model.AnswerText;
+                    if (!q.DateAnswered.HasValue) q.DateAnswered = DateTime.Now;
+                }
+
+                await this._dc.SaveChangesAsync();
+                return this.RedirectToAction(
+                    actionName: "Question",
+                    controllerName: "Home",
+                    routeValues: new { questionId = q.Id });
+            }
+            return this.View(model);
+        }
+    }
+}
---
+++ Altairis.AskMe.Web.Mvc/Models/Admin/IndexModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Xunit;
+
+namespace Altairis.AskMe.Web.Mvc.Models.Admin {
+    public class IndexModel {
+        [Required(ErrorMessage = "Není zadána otázka"), MaxLength(500), DataType(DataType.MultilineText)]
+        public string QuestionText { get; set; }
+
+        public string AnswerText { get; set; }
+
+        [MaxLength(100)]
+        public string DisplayName { get; set; }
+
+        [MaxLength(100), DataType(DataType.EmailAddress, ErrorMessage = "Nesprávný formát e-mailové adresy")]
+        public string EmailAddress { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
