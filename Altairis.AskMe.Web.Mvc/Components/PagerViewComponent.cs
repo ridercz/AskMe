53be0c9db671afd8a7a80c62f9479318f118e5f5
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-11-13 08:58:02 +0100

    Using synchronous invoke in view components.

--- Altairis.AskMe.Web.Mvc/Components/PagerViewComponent.cs
+++ Altairis.AskMe.Web.Mvc/Components/PagerViewComponent.cs
@@ -5,11 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Altairis.AskMe.Web.Mvc.Components {
     public class PagerViewComponent : ViewComponent {
 
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        public async Task<IViewComponentResult> InvokeAsync(PagingInfo model) {
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-            return this.View(model);
-        }
+        public IViewComponentResult Invoke(PagingInfo model) => this.View(model);
 
     }
 }
--- Altairis.AskMe.Web.RazorPages/Components/MenuViewComponent.cs
+++ Altairis.AskMe.Web.RazorPages/Components/MenuViewComponent.cs
@@ -3,14 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Altairis.AskMe.Web.RazorPages.Components {
     public class MenuViewComponent : ViewComponent {
-
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        public async Task<IViewComponentResult> InvokeAsync() {
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        public IViewComponentResult Invoke() {
             if (!this.User.Identity.IsAuthenticated) {
                 return this.View("Anonymous");
-            }
-            else {
+            } else {
                 return this.View("Authenticated");
             }
         }
