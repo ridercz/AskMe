f87ed3cdd1d5fb2360fa26b6df2612b92fddb2ac
Michal Altair Valášek <github@rider.cz>
2022-12-27 23:19:43 +0100

    Remove unnecessary usings.

--- Altairis.AskMe.Web.RazorPages/Controllers/SyndicationController.cs
+++ Altairis.AskMe.Web.RazorPages/Controllers/SyndicationController.cs
@@ -3,7 +3,6 @@ using System.Text.Encodings.Web;
 using System.Xml;
 using Microsoft.SyndicationFeed;
 using Microsoft.SyndicationFeed.Rss;
-using System;
 
 namespace Altairis.AskMe.Web.RazorPages.Controllers;
 
